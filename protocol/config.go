99997906c25df8291ec82c3abebdfe88d7ba11b8
Leonardo Rossi Leao <leonardo.leao@cnpem.br>
2026-08-06 09:59:17 -0300

    feat: adding functions to get and set gain and offset

---
+++ protocol/config.go
@@ -0,0 +1,39 @@
+package protocol
+
+import "fmt"
+
+func (m *Middleware) SetGain(channel int, gain float64) error {
+	config, ok := m.config.Channels[channel]
+	if !ok {
+		return fmt.Errorf("invalid channel, got: %d", channel)
+	}
+	config.Gain = gain
+	m.config.Channels[channel] = config
+	return nil
+}
+
+func (m *Middleware) GetGain(channel int) (float64, error) {
+	config, ok := m.config.Channels[channel]
+	if !ok {
+		return 0.0, fmt.Errorf("invalid channel, got: %d", channel)
+	}
+	return config.Gain, nil
+}
+
+func (m *Middleware) SetOffset(channel int, offset float64) error {
+	config, ok := m.config.Channels[channel]
+	if !ok {
+		return fmt.Errorf("invalid channel, got: %d", channel)
+	}
+	config.Offset = offset
+	m.config.Channels[channel] = config
+	return nil
+}
+
+func (m *Middleware) GetOffset(channel int) (float64, error) {
+	config, ok := m.config.Channels[channel]
+	if !ok {
+		return 0.0, fmt.Errorf("invalid channel, got: %d", channel)
+	}
+	return config.Gain, nil
+}
